78e8fdc93abcccf76a75f8908eba2b246e007a27
dcl <123@.com>
2022-05-29 16:36:22 +0800

    增加Person类！！！

---
+++ TestModule01/src/com/dcl/Person.java
@@ -0,0 +1,12 @@
+package com.dcl;
+
+/**
+ * @Auther: DCL
+ * @Date: 2022/5/29 - 05 - 29 - 16:35
+ * @Description: com.dcl
+ * @version: 1.0
+ */
+public class Person {
+    private String name;
+    private int age;
+}
